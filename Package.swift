b5ee735174bb87e4a68b091461ccaa9a3b414d94
Daniel Bratell <bratell@opera.com>
2024-04-03 14:51:34 +0200

    Check in the zip file with the framework with the rest

--- Package.swift
+++ Package.swift
@@ -25,7 +25,7 @@ let package = Package(
         ),
         .binaryTarget(
             name: "WireGuardKitGo",
-            path: "Sources/WireGuardKitGo/out/WireGuardKitGo.xcframework"
+            path: "WireGuardKitGo.xcframework.zip"
         )
     ]
 )
